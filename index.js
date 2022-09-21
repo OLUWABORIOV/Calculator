5e4865e831878ec1b4d40d4cc1d26391c7ea1f84
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-09-20 14:01:57 -1200

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => { 
+    button.addEventListener('click', (e) =>)
+})
